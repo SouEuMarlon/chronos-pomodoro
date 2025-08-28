5bd39630d0ddbe0f5e00774ae8b4365e8ca15367
Marlon Menezes <marlon_menezes@outlook.com>
2025-08-28 16:11:08 -0300

    Feat: Props and Children concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 function App() {
   return (
     <>
-      <h1>Olá mundão de meu Deus!</h1>
-      <p className='read-the-docs'>
+      <Heading>Olá mundão!!!</Heading>
+      <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque odio
         sint sed corrupti. Aut deleniti magnam officiis dolorum, ipsum
         repudiandae sed voluptate fugit architecto. Ab animi amet voluptas

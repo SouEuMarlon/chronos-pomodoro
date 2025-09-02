dca1cf1e261c2a119a452f498db190ddd5d1f534
Marlon Menezes <marlon_menezes@outlook.com>
2025-09-02 16:19:09 -0300

    Feat: Created first components (Container, Heading and Logo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
 
 function App() {
   return (
     <>
-      <Heading>Olá mundão!!!</Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque odio
-        sint sed corrupti. Aut deleniti magnam officiis dolorum, ipsum
-        repudiandae sed voluptate fugit architecto. Ab animi amet voluptas
-        incidunt officiis.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
     </>
   );
 }
--- src/components/Heading.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './Heading.module.css';
-
-export function Heading(props) {
-  console.log(props);
-  return (
-    <h1 className={`${styles.heading} ${styles.cyan}`}>{props.children}</h1>
-  );
-}

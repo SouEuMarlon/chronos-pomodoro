aec106d35bc4d19f6f774549d23be713a906def0
Marlon Menezes <marlon_menezes@outlook.com>
2025-09-03 09:51:51 -0300

    Feat: Created initial HTML for Count Down and Form sections

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 function App() {
   return (
@@ -12,6 +13,27 @@ function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <label htmlFor='input'>Task</label>
+            <input type='text' id='input' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>000000</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );

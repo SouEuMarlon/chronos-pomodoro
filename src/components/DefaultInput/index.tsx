cbfa1da03ecc3fbe5475d83bf02bbb60bfd8b92b
Marlon Menezes <marlon_menezes@outlook.com>
2025-09-12 14:48:49 -0300

    Feat: Created a Default Button

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -5,11 +5,11 @@ type DefaultInputProps = {
   label?: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, label, type, ...rest }: DefaultInputProps) {
+export function DefaultInput({ id, label, type, ...props }: DefaultInputProps) {
   return (
     <>
       {label && <label htmlFor={id}>{label}</label>}
-      <input className={styles.input} id={id} type={type} {...rest} />
+      <input className={styles.input} id={id} type={type} {...props} />
     </>
   );
 }

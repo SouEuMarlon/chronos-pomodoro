57772b2fbfd3f627cb1497d35c7d06dc2e2be068
Marlon Menezes <marlon_menezes@outlook.com>
2025-09-23 15:41:24 -0300

    Feat: Created and applyed  Task Cycle and Task Cycle type into Main Form

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,17 +3,58 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  // Ciclos
+
+  // Pega o próximo ciclo
+  const nextCycle = getNextCycle(state.currentCycle);
+
+  // Pega o tipo do próximo ciclo
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('Criando nova tarefa');
-    if (taskNameInput.current) {
-      console.log('Nome da tarefa:', taskNameInput.current.value);
-      taskNameInput.current.value = '';
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
     }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptedDate: null,
+      duration: state.config[nextCycleType]!,
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining, // Conferir depois
+        formattedSecondsRemaining: '00:00', // Conferir depois
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+    // taskNameInput.current.value = '';
   }
 
   return (
